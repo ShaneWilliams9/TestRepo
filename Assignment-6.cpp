5f78b0b441e19c9a0e172c71c1d1cc2e72744b52
ShaneWilliams9 <51734747+ShaneWilliams9@users.noreply.github.com>
2019-06-12 01:52:12 -0400

    Add test files

---
+++ Assignment-6.cpp
@@ -0,0 +1,25 @@
+/*
+
+Title: Assignment-6
+Author: Shane Williams
+Objective: Create a simple calculator with GUI using Windows Forms
+Object Description:
+MyForm2.h: Class for the form GUI and button functionality
+
+*/
+#include "pch.h"
+#include "MyForm2.h"
+#include "Windows.h"
+#include <math.h>
+#include <stdio.h>
+#include <cmath>
+
+using namespace System;
+using namespace Assignment6;
+
+int main(array<System::String ^> ^args)
+{
+	MyForm2 form;
+	form.ShowDialog();
+    return 0;
+}
